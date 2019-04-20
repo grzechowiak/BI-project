70a1bbe3340bdd6d5e0ab8ed4044429cf0c01062
grzechowiak <grzechubjj@gmail.com>
2019-04-20 17:46:54 -0500

    Update APIs.py

--- APIs.py
+++ APIs.py
@@ -13,7 +13,6 @@ links = ['https://data.austintexas.gov/api/views/hcnj-rei3/rows.csv',
          'https://data.austintexas.gov/api/views/b4k4-adkb/rows.csv',
          'https://data.austintexas.gov/resource/sxk7-7k6z.csv',
          'https://data.austintexas.gov/resource/x9yh-78fz.csv',
-         'https://data.austintexas.gov/resource/d9pb-3vh7.csv',
          'https://data.texas.gov/resource/naix-2893.csv']
 
 ######## DATA FROM THE USA GOVERNMENT ########
@@ -40,14 +39,58 @@ df6 = pd.read_csv(links[5])
 #Link to data: https://data.austintexas.gov/Building-and-Development/Issued-Construction-Permits/3syk-w9eu
 df7 = pd.read_csv(links[6])
 
-#### Residential Average Monthly kWh and Bills ####
-#Link to data: https://data.austintexas.gov/Utilities-and-City-Services/Residential-Average-Monthly-kWh-and-Bills/d9pb-3vh7
-df8 = pd.read_csv(links[7])
-
 ######## DATA FROM TEXAS GOVERNMENT ########
 ####Mixed Beverage Gross Reciepts ####
 #Link to data: https://data.texas.gov/Government-and-Taxes/Mixed-Beverage-Gross-Receipts/naix-2893
-df9 = pd.read_csv(links[8])
+df8 = pd.read_csv(links[7])
+
+
+########################################
+########## Choosing columns ############
+
+df8.head()
+len(df8.columns)
 
 
 
+
+##############################################################################
+############################ OLD MERGING CODE #######################################
+#############################################################################
+
+
+########################################
+########## Merging ##################### 
+#DF1
+df1.head()
+#Check NA values
+df1.count()
+#Zip Code has NA, delete NA only if NA is found in column Zip Code
+df1.dropna(subset=['Zip Code'], inplace = True)
+# Change data type 
+df1["Zip Code"] = df1["Zip Code"].astype(int)
+df1.dtypes
+
+#DF2
+df2.head()
+#Check NA values
+df2.count()
+
+#MERGE df1 + df2
+#Merge right join. We want everything from df2 and join to it df1
+df1_2=pd.merge(df1, df2, how='right', left_on=['Zip Code'], right_on=['Postal Code'])
+df1_2.count()
+#We don't need column "Zip Code" from column, we're gonna use Postal Code from df2 
+df1_2=df1_2.drop(columns=["Zip Code"])
+len(df1_2.columns) # Check
+
+
+#DF3
+df3.head()
+df3.columns
+#Check NAs
+df3.count() #Wow, no NA's
+
+#MERGE df1 + df2
+#Merge right join. We want everything from df2 and join to it df1
+pd.merge(df1_2, df3, how='right', left_on=['Zip Code'], right_on=['Postal Code'])
