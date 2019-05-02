749ec22d2af17fb1b57a4bd4eb9f7d64568982f7
grzechowiak <grzechubjj@gmail.com>
2019-05-02 16:56:00 -0500

    create main

--- data_collector.py
+++ data_collector.py
@@ -7,47 +7,6 @@ from Intersect_incidents import intersect
 from data_cleaning import clean_data
 
 
-
-def import_data(path_zipcodes):
-    ########################################
-    ########## Getting real time data ############
-    links = ['https://data.austintexas.gov/api/views/hcnj-rei3/rows.csv',
-             'https://data.austintexas.gov/api/views/sxk7-7k6z/rows.csv',
-             'https://data.austintexas.gov/api/views/ecmv-9xxi/rows.csv',
-             'https://data.texas.gov/resource/naix-2893.csv',
-             'https://data.austintexas.gov/api/views/dx9v-zd7x/rows.csv']
-    
-    ######## DATA FROM THE USA GOVERNMENT ########
-    #### 2014 Housing Market Analysis Data by Zip Code #### 
-    df1 = pd.read_csv(links[0]) 
-    
-    #### Austin Water - Residential Water Consumption ####
-    df2 = pd.read_csv(links[1])
-    
-    #### Food Establishment Inspection Scores ####
-    df3 = pd.read_csv(links[2], usecols=['Zip Code', 'Score']) 
-    
-    
-    ######## DATA FROM TEXAS GOVERNMENT ########
-    ####Mixed Beverage Gross Reciepts ####
-    #Link to data: https://data.texas.gov/Government-and-Taxes/Mixed-Beverage-Gross-Receipts/naix-2893
-    df4 = pd.read_csv(links[3], usecols=['location_city', 'beer_receipts','liquor_receipts','location_zip','wine_receipts','total_receipts']) 
-    #Filter by location_city cuz it's for all Texas
-    #keep: beer_receipts,liquor_receipts,location_city,location_zip,
-    #total_receipts,wine_receipts group by zipcode and take totals 
-    
-    
-    (df1,df2,df3,df4) = clean_data(df1,df2,df3,df4)
-    
-    #### Real-Time Traffic Incident Reports ####
-    df5 = pd.read_csv(links[4], usecols=['Issue Reported', 'Latitude', 'Longitude']) 
-    
-    #get grouped incidents data by zipcodes
-    path_zipcodes = "/Users/mikaelapisanileal/Documents/BI-project/files/Zipcodes/"
-    df5 = intersect(df5, path_zipcodes)
-    
-    return (df1,df2,df3,df4,df5)
-    
 def check_fix_na(data):
     missing_data = data.isnull() #localize NULLs
     missing_data.head(5)
@@ -85,5 +44,48 @@ def merge_data(df1,df2,df3,df4, df5):
     df_merged = check_fix_na(df_merged)
     
     return df_merged
+
+def import_process_data(path_zipcodes):
+    ########################################
+    ########## Getting real time data ############
+    links = ['https://data.austintexas.gov/api/views/hcnj-rei3/rows.csv',
+             'https://data.austintexas.gov/api/views/sxk7-7k6z/rows.csv',
+             'https://data.austintexas.gov/api/views/ecmv-9xxi/rows.csv',
+             'https://data.texas.gov/resource/naix-2893.csv',
+             'https://data.austintexas.gov/api/views/dx9v-zd7x/rows.csv']
+    
+    ######## DATA FROM THE USA GOVERNMENT ########
+    #### 2014 Housing Market Analysis Data by Zip Code #### 
+    df1 = pd.read_csv(links[0]) 
+    
+    #### Austin Water - Residential Water Consumption ####
+    df2 = pd.read_csv(links[1])
+    
+    #### Food Establishment Inspection Scores ####
+    df3 = pd.read_csv(links[2], usecols=['Zip Code', 'Score']) 
+    
+    
+    ######## DATA FROM TEXAS GOVERNMENT ########
+    ####Mixed Beverage Gross Reciepts ####
+    #Link to data: https://data.texas.gov/Government-and-Taxes/Mixed-Beverage-Gross-Receipts/naix-2893
+    df4 = pd.read_csv(links[3], usecols=['location_city', 'beer_receipts','liquor_receipts','location_zip','wine_receipts','total_receipts']) 
+    #Filter by location_city cuz it's for all Texas
+    #keep: beer_receipts,liquor_receipts,location_city,location_zip,
+    #total_receipts,wine_receipts group by zipcode and take totals 
+    
+    
+    (df1,df2,df3,df4) = clean_data(df1,df2,df3,df4)
+    
+    #### Real-Time Traffic Incident Reports ####
+    df5 = pd.read_csv(links[4], usecols=['Issue Reported', 'Latitude', 'Longitude']) 
+    
+    #get grouped incidents data by zipcodes
+    df5 = intersect(df5, path_zipcodes)
+    
+    merge=merge_data(df1,df2,df3,df4, df5)
+    
+    return merge
+    
+
 
     
--- main.py
+++ main.py
@@ -1,42 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May  2 13:40:40 2019
+from data_collector import import_process_data
 
-@author: mikaelapisanileal
-"""
+def main():
+    path_zip=input('Enter the path to the folder called Zipcodes (it is in files folder)')
+    output=input('Enter where you wanna save your file')
+    
+    final_df=import_process_data(path_zip)
+    final_df.to_csv(output + 'final_data.csv')
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-Created on Mon Mar 25 16:01:57 2019
-
-@author: mikaelapisanileal
-"""
-
-import sys
-import getopt
-from APIs import import_data
-
-def info():
-    print('main.py -p <path>')
-
-def main(argv):
-   path_zipcodes = ''
-   try:
-      opts, args = getopt.getopt(argv,'hp:',['path_zipcodes='])
-   except getopt.GetoptError:
-      info()
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         info()
-         sys.exit()
-      elif opt in ('-p', '--path_zipcodes'):
-         path_zipcodes = arg
-        
-   import_data(path_zipcodes)
-   
-if __name__ == "__main__":
-    main(sys.argv[1:])
+main()
